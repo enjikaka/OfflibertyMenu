c542eddf242a61e510390763d8f246091e38c5d4
Jeremy Karlsson <hello@jeremy.se>
2015-10-27 03:34:51 +0100

    Removed offliberty and added youtubeinmp3

--- app.js
+++ app.js
@@ -1,56 +1,13 @@
-var offliberty  = require('offliberty'),
-    open        = require('open'),
-    remote      = require('remote'),
-    app         = remote.require('app'),
-    canHint     = true;
-
-function reset() {
-  setText('Offliberty');
-  setBackground('#ff5722');
-
-  document.body.classList.remove('loading');
-  document.querySelector('#url').value = null;
-}
-
-function setText(string) {
-  document.querySelector('h1').textContent = string;
-}
-
-function setBackground(color) {
-  document.querySelector('header').style.backgroundColor = color;
-}
+var open        = require('open');
+var remote      = require('remote');
+var app         = remote.require('app');
 
 document.querySelector('#close-button').addEventListener('click', function() {
   app.quit();
 });
 
 document.querySelector('#download-song').addEventListener('click', function() {
-  document.body.classList.add('loading');
-  setText('Loading...');
-
-  setTimeout(function() {
-    if (canHint) {
-      setText('You can leave this window while waiting...');
-    }
-  }, 5000);
-
   var youtubeUrl = document.querySelector('#url').value;
-
-  offliberty.off(youtubeUrl, function(error, downloadLink) {
-    if (!error) {
-      open(downloadLink);
-      reset();
-    } else {
-      canHint = false;
-
-      setBackground('#f44336');
-      setText('Error, sorry.');
-
-      setTimeout(function() {
-        reset();
-      }, 5000);
-
-      console.error(err);
-    }
-  });
+  open('http://youtubeinmp3.com/fetch/?video=' + youtubeUrl);
+  document.querySelector('#url').value = null;
 });
--- main.js
+++ main.js
@@ -1,12 +1,34 @@
-var menubar = require('menubar');
+var osxMenubar = require('menubar');
+var Menu = require('menu');
+var app = require('app');
 
-var menu = menubar({
+var menuTemplate = [{
+    label: "Application",
+    submenu: [
+        { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+        { type: "separator" },
+        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+    ]}, {
+    label: "Edit",
+    submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]}
+];
+
+var osxBar = osxMenubar({
 	width: 400,
 	height: 100,
-	dir: __dirname,
-	showDockIcon: true
+	dir: __dirname
 });
 
-menu.on('ready', function() {
+osxBar.on('ready', function() {
 	console.log('App is ready');
+
+	Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
 });
